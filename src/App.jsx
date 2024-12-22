2964ef006bd8a2f4d53933e9b8d09c09c22b05f5
Your Name <youremail@example.com>
2024-12-22 19:57:46 +0530

    updated the import path for correct case sensitive file path for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/header"
+import Header from "./components/Header"
 import Body from "./components/Body"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 
 
 function App() {
