1037839e7c6b773b52692e2517fbca0265713553
Your Name <youremail@example.com>
2024-12-22 13:18:01 +0530

    making all the files ready to push to main branch

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/header"
+import Body from "./components/Body"
+import Footer from "./components/footer"
+
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-amber-50 min-h-screen px-6 py-3 flex flex-col gap-16 md:px-10 md:py-7 md:justify-between lg:px-20 lg:py-16">
+        <Header></Header>
+        <Body></Body>
+        <Footer></Footer>
+      </div>
+    </>
+  )
+}
+
+export default App
